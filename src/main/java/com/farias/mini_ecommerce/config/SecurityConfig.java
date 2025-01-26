a6548dd8df065c4756060edf7a5064c86a08c9dd
Farias <pablogarciafarias@gmail.com>
2025-01-26 19:57:25 -0300

    docker-compose.yml

--- src/main/java/com/farias/mini_ecommerce/config/SecurityConfig.java
+++ src/main/java/com/farias/mini_ecommerce/config/SecurityConfig.java
@@ -2,13 +2,38 @@ package com.farias.mini_ecommerce.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
+
+    private static final String[] AUTH_WHITELIST = {
+            "/v3/api-docs/**",
+            "/swagger-ui/**",
+            "/swagger-ui.html"
+    };
+
     @Bean
     public BCryptPasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
-}
 
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+                http.authorizeHttpRequests((requests) -> requests
+                        .requestMatchers(AUTH_WHITELIST).permitAll()
+                        .requestMatchers("/v1/product/**").permitAll()
+                        .requestMatchers("/v1/product/create").permitAll()
+                        .requestMatchers("/v1/user/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .logout(LogoutConfigurer::permitAll);
+
+        return http.build();
+    }
+}
